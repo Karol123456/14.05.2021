e2da816a8524ff817164c95f21bf0ce71d12dc75
KacDzi <kacper.dziebor@tlen.pl>
2021-05-14 14:50:28 +0200

    ff
    fffffffffffffffff

---
+++ main.js
@@ -0,0 +1,212 @@
+
+
+const gridMain = document.createElement("div")
+gridMain.setAttribute('class' , 'grid')
+
+const item1 = document.createElement("div");
+item1.setAttribute( 'class' , 'item')
+
+const tytuł1 = document.createElement("h1");
+tytuł1.setAttribute( 'class' , 'tytuł1')
+tytuł1.innerText = 'book 1' 
+
+
+const ikona1 = document.createElement("i");
+ikona1.setAttribute( 'class' , 'fa fa-close ikona1')
+
+const book1 = document.createElement("p");
+book1.setAttribute( 'class' , 'book 1')
+book1.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse natus quos qui deserunt, earum totam amet dicta nobis minus tempora quaerat porro rem culpa saepe. Dolorem rerum cum illo perferendis!Praesentium neque eligendi eius minus quam consectetur, ratione facilis '
+
+const buttonU = document.createElement("button");
+buttonU.setAttribute( 'class' , 'przyciskU')
+buttonU.innerText = 'USUŃ'
+
+const buttonE = document.createElement("button");
+buttonE.setAttribute( 'class' , 'przyciskE')
+buttonE.innerText = 'EDYTUJ'
+
+//
+const item2 = document.createElement("div");
+item2.setAttribute( 'class' , 'item')
+
+const tytuł2 = document.createElement("h1");
+tytuł2.setAttribute( 'class' , 'tytuł1')
+tytuł2.innerText = 'book 2'
+
+const ikona2 = document.createElement("i");
+ikona2.setAttribute( 'class' , 'fa fa-close ikona1')
+
+const book2 = document.createElement("p");
+book2.setAttribute( 'class' , 'book 1')
+book2.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse natus quos qui deserunt, earum totam amet dicta nobis minus tempora quaerat porro rem culpa saepe. Dolorem rerum cum illo perferendis!Praesentium neque eligendi eius minus quam consectetur, ratione facilis '
+
+const buttonU1 = document.createElement("button");
+buttonU1.setAttribute( 'class' , 'przyciskU')
+buttonU1.innerText = 'USUŃ'
+
+const buttonE1 = document.createElement("button");
+buttonE1.setAttribute( 'class' , 'przyciskE')
+buttonE1.innerText = 'EDYTUJ'
+//
+const item3 = document.createElement("div");
+item3.setAttribute( 'class' , 'item')
+
+const tytuł3 = document.createElement("h1");
+tytuł3.setAttribute( 'class' , 'tytuł1')
+tytuł3.innerText = 'book 3'
+
+const ikona3 = document.createElement("i");
+ikona3.setAttribute( 'class' , 'fa fa-close ikona1')
+
+const book3 = document.createElement("p");
+book3.setAttribute( 'class' , 'book1')
+book3.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse natus quos qui deserunt, earum totam amet dicta nobis minus tempora quaerat porro rem culpa saepe. Dolorem rerum cum illo perferendis!Praesentium neque eligendi eius minus quam consectetur, ratione facilis '
+
+const buttonU2 = document.createElement("button");
+buttonU2.setAttribute( 'class' , 'przyciskU')
+buttonU2.innerText = 'USUŃ'
+
+const buttonE2 = document.createElement("button");
+buttonE2.setAttribute( 'class' , 'przyciskE')
+buttonE2.innerText = 'EDYTUJ'
+//
+const item4 = document.createElement("div");
+item4.setAttribute( 'class' , 'item')
+
+const tytuł4 = document.createElement("h1");
+tytuł4.setAttribute( 'class' , 'tytuł1')
+tytuł4.innerText = 'book 4'
+
+const ikona4 = document.createElement("i");
+ikona4.setAttribute( 'class' , 'fa fa-close ikona1')
+
+const book4 = document.createElement("p");
+book4.setAttribute( 'class' , 'book1' )
+book4.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse natus quos qui deserunt, earum totam amet dicta nobis minus tempora quaerat porro rem culpa saepe. Dolorem rerum cum illo perferendis!Praesentium neque eligendi eius minus quam consectetur, ratione facilis '
+
+const buttonU3 = document.createElement("button");
+buttonU3.setAttribute( 'class' , 'przyciskU')
+buttonU3.innerText = 'USUŃ'
+
+const buttonE3 = document.createElement("button");
+buttonE3.setAttribute( 'class' , 'przyciskE')
+buttonE3.innerText = 'EDYTUJ'
+//
+const item5 = document.createElement("div");
+item5.setAttribute( 'class' , 'item')
+
+const tytuł5 = document.createElement("h1");
+tytuł5.setAttribute( 'class' , 'tytuł1')
+tytuł5.innerText = 'book 5'
+
+const ikona5 = document.createElement("i");
+ikona5.setAttribute( 'class' , 'fa fa-close ikona1')
+
+const book5 = document.createElement("p");
+book5.setAttribute( 'class' , 'book1')
+book5.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse natus quos qui deserunt, earum totam amet dicta nobis minus tempora quaerat porro rem culpa saepe. Dolorem rerum cum illo perferendis!Praesentium neque eligendi eius minus quam consectetur, ratione facilis '
+
+const buttonU4 = document.createElement("button");
+buttonU4.setAttribute( 'class' , 'przyciskU')
+buttonU4.innerText = 'USUŃ'
+
+const buttonE4 = document.createElement("button");
+buttonE4.setAttribute( 'class' , 'przyciskE')
+buttonE4.innerText = 'EDYTUJ'
+//
+const item6 = document.createElement("div");
+item6.setAttribute( 'class' , 'item')
+
+const tytuł6 = document.createElement("h1");
+tytuł6.setAttribute( 'class' , 'tytuł1')
+tytuł6.innerText = 'book 6'
+
+const ikona6 = document.createElement("i");
+ikona6.setAttribute( 'class' , 'fa fa-close ikona1')
+
+const book6 = document.createElement("p");
+book6.setAttribute( 'class' , 'book1')
+book6.innerText = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse natus quos qui deserunt, earum totam amet dicta nobis minus tempora quaerat porro rem culpa saepe. Dolorem rerum cum illo perferendis!Praesentium neque eligendi eius minus quam consectetur, ratione facilis '
+
+const buttonU5 = document.createElement("button");
+buttonU5.setAttribute( 'class' , 'przyciskU')
+buttonU5.innerText = 'USUŃ'
+
+const buttonE5 = document.createElement("button");
+buttonE5.setAttribute( 'class' , 'przyciskE')
+buttonE5.innerText = 'EDYTUJ'
+
+
+
+
+
+
+gridMain.appendChild(item1)
+item1.appendChild(ikona1)
+gridMain.appendChild(item1)
+item1.appendChild(tytuł1)
+gridMain.appendChild(item1)
+item1.appendChild(book1)
+gridMain.appendChild(item1)
+item1.appendChild(buttonU)
+gridMain.appendChild(item1)
+item1.appendChild(buttonE)
+
+gridMain.appendChild(item2)
+item2.appendChild(ikona2)
+gridMain.appendChild(item2)
+item2.appendChild(tytuł2)
+gridMain.appendChild(item2)
+item2.appendChild(book2)
+gridMain.appendChild(item2)
+item2.appendChild(buttonU1)
+gridMain.appendChild(item2)
+item2.appendChild(buttonE1)
+
+gridMain.appendChild(item3)
+item3.appendChild(ikona3)
+gridMain.appendChild(item3)
+item3.appendChild(tytuł3)
+gridMain.appendChild(item3)
+item3.appendChild(book3)
+gridMain.appendChild(item3)
+item3.appendChild(buttonU2)
+gridMain.appendChild(item3)
+item3.appendChild(buttonE2)
+
+gridMain.appendChild(item4)
+item4.appendChild(ikona4 )
+gridMain.appendChild(item4)
+item4.appendChild(tytuł4)
+gridMain.appendChild(item4)
+item4.appendChild(book4)
+gridMain.appendChild(item4)
+item4.appendChild(buttonU3)
+gridMain.appendChild(item4)
+item4.appendChild(buttonE3)
+
+gridMain.appendChild(item5)
+item5.appendChild(ikona5)
+gridMain.appendChild(item5)
+item5.appendChild(tytuł5)
+gridMain.appendChild(item5)
+item5.appendChild(book5)
+gridMain.appendChild(item5)
+item5.appendChild(buttonU4)
+gridMain.appendChild(item5)
+item5.appendChild(buttonE4)
+
+gridMain.appendChild(item6)
+item6.appendChild(ikona6)
+gridMain.appendChild(item6)
+item6.appendChild(tytuł6)
+gridMain.appendChild(item6)
+item6.appendChild(book6)
+gridMain.appendChild(item6)
+item6.appendChild(buttonU5)
+gridMain.appendChild(item6)
+item6.appendChild(buttonE5)
+
+
+app.appendChild(gridMain)
